e148b6f3ca43b60dd90dfde95b8c273b5ed95419
San Kang <sankang@Sans-MacBook-Pro.local>
2017-08-23 12:11:21 -0700

    fixed some bugs and added custome filename and auto-detecting number of tags

--- rank-tag/README.md
+++ rank-tag/README.md
@@ -4,11 +4,9 @@ This Script allows to parse Tag ID along with the ranking (popularity).
 
 ## Getting Started
 
-* The Name of the excel file **must** be "tempfile.xlsx".
-
 * It would parse from the **second tab** in the excel file.
 
-* To Run this, ```MONGO_URI="your-mongodb-uri" node rank-tag```
+* To Run this, ```MONGO_URI="your-mongodb-uri" node rank-tag your-excel-file-name```
 
 * It is replacing a document in the MongoDB, so you should change ObjectId if the database changes.
 
--- rank-tag/rank-tag.js
+++ rank-tag/rank-tag.js
@@ -6,13 +6,32 @@ var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
 var url = process.env.MONGO_URI;
 
-var tagsInRank = {}
+// Needs Excel File name
+var file = (process.argv.slice(2))[0];
 
-// Parse Excel File 
-parseXlsx('tempfile.xlsx', '2', function(err, data) {
+
+var tagsInRank = {}
+var numberOfTags = 0;
+
+// Parsing from the second tab
+parseXlsx(file, '2', function(err, data) {
 	if(err) throw err;
-	for(var i = 1; i <= 962; i++) {
-		// Assuming Tag ID is on first column (starting from second row)
+
+	// gets for the total number of tags
+	for (var j=0; j < data.length; j++) {
+		if(data[j][1] === "TOTAL") {
+			numberOfTags = parseInt(data[j][0]);
+		} 
+	}
+
+	// error handling when there is no number of total tags
+	if (!numberOfTags) {
+		console.log("There should be a number of total tags left to the 'TOTAL'");
+		process.exit(0);
+	}
+
+	// Assuming Tag ID is on first column (starting from second row)
+	for(var i = 1; i <= numberOfTags; i++) {
 		tagsInRank[((data[i][0]).substring(0,(data[i][0]).length - 2))] = i;
 	}
 
@@ -21,7 +40,7 @@ parseXlsx('tempfile.xlsx', '2', function(err, data) {
 	tagsInRank.date = date.toDateString();
 
 	// Debugging Purpose
-	//	fs.writeFile('temp.json', JSON.stringify(tagsInRank), function (err) {});
+	// fs.writeFile('temp.json', JSON.stringify(tagsInRank), function (err) {});
 
 
 	// Connect Mongo and replace it a masterTag document in masterTag
@@ -37,5 +56,6 @@ parseXlsx('tempfile.xlsx', '2', function(err, data) {
 	    	});
    	 	}
 	});
+
 });
 
